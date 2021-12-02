797ea012cadf5c2ea59f4622bb52300264430b1b
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-02 05:24:26 -0800

    Finished several algorithms

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,9 @@
 /* exported reverseWord */
+
+function reverseWord(word) {
+  let reversal = '';
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversal += word[i];
+  }
+  return reversal;
+}
