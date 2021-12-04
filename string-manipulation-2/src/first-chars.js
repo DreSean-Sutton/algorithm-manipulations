8aab7561da255bef693f4dd20ed72df1f4ef11a2
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-03 21:16:56 -0800

    Finished a couple algorithms

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,27 @@
 /* exported firstChars */
+
+/*
+-assign an empty string to variable primaryChars
+-create a for loop that
+  a. assigns 0 to i
+  b. executes if i is less than length
+  c. i++
+-primaryChars += string[i]
+return primaryChars
+*/
+
+function firstChars(length, string) {
+  let primaryChars = '';
+
+  if (!string) {
+    return '';
+  }
+
+  if (string.length < length) {
+    return string;
+  }
+  for (let i = 0; i < length; i++) {
+    primaryChars += string[i];
+  }
+  return primaryChars;
+}
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,38 @@
 /* exported numVowels */
+/*
+-assign zero to variable vowels
+-create a for loop that
+  a. assigns 0 to i
+  b. executes if i is less than string.length
+  c. i++
+-if string[i] is strictly equal to any uppercase or lowercase vowels
+  vowels++
+-return vowels
+*/
+// potentially use a variable to count for
+// vowels and increment it each time
+
+function numVowels(string) {
+  let vowels = '';
+  if (!string) {
+    return 0;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    if (
+      string[i] === 'a' ||
+      string[i] === 'A' ||
+      string[i] === 'e' ||
+      string[i] === 'E' ||
+      string[i] === 'i' ||
+      string[i] === 'I' ||
+      string[i] === 'o' ||
+      string[i] === 'O' ||
+      string[i] === 'u' ||
+      string[i] === 'U'
+    ) {
+      vowels++;
+    }
+  }
+  return vowels;
+}
