567741095b5df6f9f22255bd724a1464441335af
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-29 03:51:52 -0800

    FInished flatten algorithm

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,19 @@
 /* exported flatten */
+
+/*
+-
+*/
+
+function flatten(array) {
+  const flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!Array.isArray(array[i])) {
+      flattenedArray.push(array[i]);
+    } else {
+      for (let j = 0; j < array[i].length; j++) {
+        flattenedArray.push(array[i][j]);
+      }
+    }
+  }
+  return flattenedArray;
+}
