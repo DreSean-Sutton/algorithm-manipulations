ae901c6068e2a96ead777eb93e35c1b37f35c00b
DreSean-Sutton <dreseansutton@gmail.com>
2021-12-31 15:36:17 -0800

    Finished difference and unique algorithms

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,22 @@
 /* exported unique */
+
+/*
+-create a variable named uniqueArr and assign it the value of an empty array
+-create a for loop that:
+  a. assign the value of 0 to i
+  b. executes code block if i is less than array.length
+  c. i++
+  -if uniqueArr doesn't include array[i]
+    push array[i] into uniqueArr
+return uniqueArr
+*/
+
+function unique(array) {
+  const uniqueArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!uniqueArr.includes(array[i])) {
+      uniqueArr.push(array[i]);
+    }
+  }
+  return uniqueArr;
+}
